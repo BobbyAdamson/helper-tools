f3601403f58f6ee443141c886ebbba715f5f59c4
Bobby Adamson <bobby@bobbyadamson.com>
2015-08-13 21:16:53 -0400

    Adding: Iframe

---
+++ iframe-page/index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+
+<form action="<?php $_SERVER['PHP_SELF']; ?>">
+	<div class="inner">
+		<label>Source URL for your script embed</label>
+		<textarea name="code" id="code"></textarea>
+		<button type="submit">Submit</button>
+		<a href="#" class="closeForm">Close</a>
+	</div>
+</form>
+
+<?php
+$code = '';
+if(isset($_GET['code'])) {
+	$code = $_GET['code'];
+}
+if(isset($_POST['code'])) {
+	$code = $_POST['code'];
+	http_build_query(array_merge($_GET, array('code'=>$_GET['code'])));
+}
+if($code != '') {
+	echo '<script src="' . $code . '"></script>';
+}
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script>
+	$(document).ready(function(){
+		$('body').on('click', '.closeForm', function(){
+			if($('form').hasClass('xHidden')) {
+				$('form').removeClass('xHidden');
+			} else {
+				$('form').addClass('xHidden');
+			}
+			return false;
+		});
+
+		if(typeof NGX != 'undefined') {
+			$('form').addClass('xHidden');
+
+			$.ajax({
+				url: 'https://api-ssl.bitly.com/v3/shorten',
+				type: 'get',
+				data: {
+					'access_token': 'ccce0769d3cab20dd9b4cf5cb56e9549b9d661db',
+					'longUrl': window.location.href,
+					'format': 'txt'
+				},
+				success: function(msg) {
+					console.log(msg);
+					$('form .inner').append('<div class="bitlyLink"><p>Your bit.ly link: <input value="' + msg +'"></input></p></div>');
+					$('form').addClass('bitlyLink');
+				}
+			});
+		}
+	});
+</script>
+	</body>
+</html>
